76f8615395ff9532081938c3d623232851e1cf50
dragoteon <woest015@morris.umn.edu>
2017-11-03 14:11:32 -0500

    comments

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,12 +4,15 @@ import java.io.*;
 
 public class EchoClient {
 	public static void main(String[] args) {
-		try { 
+		try {
+		       //setup socket	
 			Socket socket = new Socket("127.0.0.1", 6013);
 			
+			//setup datastreams
 			OutputStream outstream = socket.getOutputStream();
 			InputStream instream = socket.getInputStream();
 
+			//byte to be written 
 			byte[] written = new byte[1];
 
 			while(instream.read(written) != -1) {
@@ -19,9 +22,12 @@ public class EchoClient {
 				instream.read(printed);
 				System.out.write(printed, 0, 1);
 			}
+
+			//close
 			instream.close();
 			outstream.close();
 		} catch (IOException err) {
+			//terrible error handling 
 			System.out.println("Yaaaay failure");
 			System.err.println(err);
 		}
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class EchoServer {
 	public static void main(String[] args) {
 	try {
+		//See EchoClient because the comments are almost identical
 		ServerSocket socket = new ServerSocket(6013);
 		byte[] in = new byte[1];
 		while(true) {
