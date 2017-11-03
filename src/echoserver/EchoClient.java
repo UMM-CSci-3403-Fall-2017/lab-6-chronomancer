1d9bc1926649b5e23493654257e083eec62bc3e4
dragoteon <woest015@morris.umn.edu>
2017-11-03 14:08:25 -0500

    finished server files need comments.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,30 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static void main(String[] args) {
+		try { 
+			Socket socket = new Socket("127.0.0.1", 6013);
+			
+			OutputStream outstream = socket.getOutputStream();
+			InputStream instream = socket.getInputStream();
+
+			byte[] written = new byte[1];
+
+			while(instream.read(written) != -1) {
+				outstream.write(written);
+
+				byte[] printed = new byte[1];
+				instream.read(printed);
+				System.out.write(printed, 0, 1);
+			}
+			instream.close();
+			outstream.close();
+		} catch (IOException err) {
+			System.out.println("Yaaaay failure");
+			System.err.println(err);
+		}
+	}
+			
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,26 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static void main(String[] args) {
+	try {
+		ServerSocket socket = new ServerSocket(6013);
+		byte[] in = new byte[1];
+		while(true) {
+		Socket socket_client = socket.accept();
+
+		InputStream IN = socket_client.getInputStream();
+		OutputStream out = socket_client.getOutputStream();
+	
+		while(IN.read(in) != -1) {
+			out.write(in);
+		}
+		}
+
+	} catch (IOException err) {
+		System.out.println("yay more failure");
+		System.err.println(err);
+	}
+}
 }
